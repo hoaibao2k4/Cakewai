26484f5e7d69193ea681d01fe091c4861a4a8938
hoaibao2k4 <hoaibaodongthap@gmail.com>
2024-10-18 14:44:24 +0700

    Fix homepage and create auth layout

--- src/components/Layouts/components/Header/index.jsx
+++ src/components/Layouts/components/Header/index.jsx
@@ -1,4 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 function Header() {
+  
+  const navigator = useNavigate();
+  const handleLogin = () => {
+    navigator("/auth?mode=signin");
+  };
+
   return (
     <header className="fixed left-1/2 top-0 z-10 h-16 w-full max-w-screen-xl -translate-x-1/2 transform bg-primary">
       <div className="mx-20 flex h-full items-center justify-between text-white">
@@ -62,13 +70,15 @@ function Header() {
           </li>
         </ul>
         <div className="flex gap-6">
+          {/* Cart Logo */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="size-6"
+            class="size-6 cursor-pointer"
+            onClick={() => viewCart()}
           >
             <path
               stroke-linecap="round"
@@ -76,14 +86,15 @@ function Header() {
               d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
             />
           </svg>
-
+          {/* User Logo */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="size-6"
+            class="size-6 cursor-pointer"
+            onClick={() => handleLogin()}
           >
             <path
               stroke-linecap="round"
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,12 +3,14 @@ import GenImage from "../pages/Image Generator";
 import About from "../pages/About/about";
 import Discount from "../pages/Discount";
 import Category from "../pages/Categories/category";
+import Auth from "~/pages/authentication/auth";
 const publicRoute = [
   { path: "/", component: Home },
   { path: "/generator", component: GenImage },
   { path: "/about", component: About},
   { path: "/discount", component: Discount},
   { path: "/category", component: Category},
+  { path: "/auth", component: Auth},
 ];
 
 const privateRoute = [];
