45a0a4b74ca8d85353d3c08543deb9a08722252b
ManGia44 <vienthongphuhung1@gmail.com>
2024-10-30 00:36:28 +0700

    updated footer version 1

--- src/components/Layouts/components/Footer/index.jsx
+++ src/components/Layouts/components/Footer/index.jsx
@@ -1,12 +1,17 @@
 function Footer() {
   return (
-    <footer className="flex h-[540px] w-full justify-evenly bg-primary text-white">
+    <footer className="relative flex h-[540px] w-full justify-evenly bg-white text-black border-t-2">
       <div className="my-20">
-        <img src="./src/assets/images/baker.jpg" alt="Cake with AI" className="rounded-lg" />
-        <div className="mt-5">
+        <img src="./src/assets/images/logo.svg" alt="Cake with AI" className="mt-12" />
+        <div className="mt-2">
           <h2>INFO TEXT</h2>
           <address>Address: UIT</address>
           <span>Tel: 0939666666</span>
+          <div className="flex space-x-2 mt-2">
+          <img src="./src/assets/images/facebook.png" alt="facebook" />
+          <img src="./src/assets/images/tiktok-16.png" alt="tiktok" />
+          <img src="./src/assets/images/instagram-16.png" alt="instagram" />
+          </div>        
         </div>
       </div>
       <div className="my-32">
@@ -44,6 +49,10 @@ function Footer() {
           <li>Menu con</li>
           <li>Menu con</li>
         </ul>
+      </div>
+     
+      <div className="absolute bottom-12  justify-center flex items-end border-t-2 py-4 mx-auto w-[60%]">
+      Copyright © 2024 CAKEWAI | All Rights Reserved | <a href="#" className="underline hover:text-green-500">Terms and Conditions</a> | <a href="#" className="underline hover:text-green-500">Privacy Policy</a>
       </div>
     </footer>
   );
