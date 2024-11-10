f596514c5f799a2982b82885d9e4e5c30622971b
Phan Huu Phuoc Loc <109939299+locphp@users.noreply.github.com>
2024-11-10 12:51:57 +0700

    feat(cart): Completed static UI for the cart page

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,15 +6,17 @@ import Category from "../pages/Categories/category";
 import Auth from "~/pages/authentication/auth";
 import Policy from "~/pages/Policy";
 import DetailedCake from "~/components/Layouts/components/DetailedCake";
+import Cart from "~/components/Layouts/components/Cart";
 const publicRoute = [
   { path: "/", component: Home },
   { path: "/generator", component: GenImage },
-  { path: "/about", component: About},
-  { path: "/news", component: News},
-  { path: "/category", component: Category},
-  { path: "/auth", component: Auth},
-  { path: "/policy", component: Policy},
-  { path: "/detailed", component: DetailedCake},
+  { path: "/about", component: About },
+  { path: "/news", component: News },
+  { path: "/category", component: Category },
+  { path: "/auth", component: Auth },
+  { path: "/policy", component: Policy },
+  { path: "/detailed", component: DetailedCake },
+  { path: "/cart", component: Cart },
 ];
 
 const privateRoute = [];
