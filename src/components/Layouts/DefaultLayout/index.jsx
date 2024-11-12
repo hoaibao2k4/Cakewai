2347420a050a5885a359ffe21595a83c3808526d
Phan Huu Phuoc Loc <109939299+locphp@users.noreply.github.com>
2024-11-12 13:58:16 +0700

    fix screen

--- src/components/Layouts/DefaultLayout/index.jsx
+++ src/components/Layouts/DefaultLayout/index.jsx
@@ -2,7 +2,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 function DefaultLayout({ children }) {
   return (
-    <div className="wrapper max-w-screen-xl">
+    <div className="wrapper w-screen-xl">
       <Header />
       <div className="">{children}</div>
       <Footer />
