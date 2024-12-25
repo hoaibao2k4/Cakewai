046fd640d61a3679f6555b1930e9ec8a30b1263a
hoaibao2k4 <hoaibaodongthap@gmail.com>
2024-12-25 09:26:58 +0700

    fix bugs

--- src/components/dashboard/NavbarVertical/index.jsx
+++ src/components/dashboard/NavbarVertical/index.jsx
@@ -1,65 +1,59 @@
-import {
-    BarChartOutlined,
-    FormOutlined,
-    ProductOutlined,
-    UserAddOutlined,
-    FileWordOutlined
-} from '@ant-design/icons';
+import { BarChartOutlined, FormOutlined, ProductOutlined, UserAddOutlined, FileWordOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const NavbarVertical = () => {
-    const [current, setCurrent] = useState(() => {
-        return localStorage.getItem("currentKey") || "dashboard";
-    });
+  const [current, setCurrent] = useState(() => {
+    return localStorage.getItem('currentKey') || 'dashboard';
+  });
 
-    useEffect(() => {
-        localStorage.setItem("currentKey", current);
-    }, [current]);
+  useEffect(() => {
+    localStorage.setItem('currentKey', current);
+  }, [current]);
 
-    const onClick = (e) => {
-        setCurrent(e.key);
-    };
+  const onClick = (e) => {
+    setCurrent(e.key);
+  };
 
-    const items = [
-        {
-            key: 'dashboard',
-            icon: <BarChartOutlined />,
-            label: (<Link to="/admin/dashboard">Thống kê</Link>),
-        },
-        {
-            key: 'product-management',
-            icon: <ProductOutlined />,
-            label: (<Link to="/admin/product_management">Quản lý sản phẩm</Link>),
-        },
-        {
-            key: 'order-management',
-            icon: <FormOutlined />,
-            label: (<Link to="/admin/order_management">Quản lý đơn hàng</Link>),
-        },
-        {
-            key: 'register-customer',
-            icon: <UserAddOutlined />,
-            label: (<Link to="/admin/register_customer">Khách hàng thành viên</Link>),
-        },
-        {
-            key: 'text-editor',
-            icon: <FileWordOutlined />,
-            label: (<Link to="/admin/text-editor">Bài viết</Link>),
-        },
-    ];
-    return (
-        <div className="w-60 h-screen">
-            <Menu
-                onClick={onClick}
-                selectedKeys={[current]}
-                style={{ height: '100%', backgroundColor: '#fcf0cf' }}
-                mode="vertical"
-                items={items}
-            />
-        </div>
-    );
-}
+  const items = [
+    {
+      key: 'dashboard',
+      icon: <BarChartOutlined />,
+      label: <Link to="/admin/dashboard">Thống kê</Link>,
+    },
+    {
+      key: 'product-management',
+      icon: <ProductOutlined />,
+      label: <Link to="/admin/product_management">Quản lý sản phẩm</Link>,
+    },
+    {
+      key: 'order-management',
+      icon: <FormOutlined />,
+      label: <Link to="/admin/order_management">Quản lý đơn hàng</Link>,
+    },
+    {
+      key: 'register-customer',
+      icon: <UserAddOutlined />,
+      label: <Link to="/admin/register_customer">Khách hàng thành viên</Link>,
+    },
+    {
+      key: 'text-editor',
+      icon: <FileWordOutlined />,
+      label: <Link to="/admin/text-editor">Bài viết</Link>,
+    },
+  ];
+  return (
+    <div className="min-h-screen w-60">
+      <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        style={{ height: '100%', backgroundColor: '#fcf0cf' }}
+        mode="vertical"
+        items={items}
+      />
+    </div>
+  );
+};
 
 export default NavbarVertical;
